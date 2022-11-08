cd933e9a9d969debbeba2186cb13f134cde7d0f3
EC2 Default User <ec2-user@ip-172-31-35-168.eu-west-1.compute.internal>
2022-11-08 10:54:08 +0000

    2

--- infra/dashboard.tf
+++ infra/dashboard.tf
@@ -21,7 +21,28 @@ resource "aws_cloudwatch_dashboard" "main" {
         "region": "eu-west-1",
         "title": "Total number of accounts"
       }
+    },
+    
+        {
+      "type": "metric",
+      "x": 0,
+      "y": 0,
+      "width": 12,
+      "height": 6,
+      "properties": {
+        "metrics": [
+          [
+            "${var.student_name}",
+            "bank_sum.value"
+          ]
+        ],
+        "period": 300,
+        "stat": "Maximum",
+        "region": "eu-west-1",
+        "title": "Total bank amount"
+      }
     }
+    
   ]
 }
 DASHBOARD
--- infra/variables.tf
+++ infra/variables.tf
@@ -1,3 +1,14 @@
 variable "student_name" {
   type = string
+  default = "hali015"
+}
+
+variable "student_email"{
+    type = string
+    default = "harry.mliam@gmail.com"
+}
+
+variable "alarm_name" {
+  type = string
+  default = "hali-alarm"
 }
